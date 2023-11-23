a9993ae665818571311d0951670e5e3139d1728a
phy46 <hyeni0406@naver.com>
2023-11-23 23:43:31 +0900

    단어 하강 속도 조절

--- TermProject/term.cpp
+++ TermProject/term.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <random>
 
 using namespace std;
 
@@ -62,6 +66,90 @@ public:
     }
 };
 
+vector<string> current_words(10, "");  // 10줄의 단어 목록
+mutex mtx;  // 벡터를 보호하기 위한 뮤텍스
+
+void drop(WordCreator* wordCreator, vector<string>& current_words) {
+    random_device rd;
+    mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(500, 2000); // 0.5초에서 2초 사이의 랜덤한 시간을 생성
+
+    while (true) {
+        this_thread::sleep_for(chrono::milliseconds(distr(gen)));  // 랜덤한 시간 동안 대기
+        mtx.lock();
+        // 맨 아래 줄부터 문자열을 하나씩 위로 올림
+        for (int i = current_words.size() - 1; i > 0; i--) {
+            current_words[i] = current_words[i - 1];
+        }
+
+        // 맨 위 줄에 새로운 문자열 추가
+        int space = rand() % 4 * 20;  // 콘솔창을 x축으로 4등분하여 랜덤한 위치에 문자열 출력
+        int length = rand() % 8 + 1;  // 단어의 길이를 랜덤하게 생성
+        current_words[0] = string(space, ' ') + wordCreator->createWord(length);
+        mtx.unlock();
+    }
+}
+
+bool isInputting = false;  // 사용자의 입력 상태를 나타내는 전역 변수
+
+void printWords(const vector<string>& current_words) {
+    while (true) {
+        if (!isInputting)
+        {
+            system("cls");  // 화면 지우기
+            mtx.lock();  // 뮤텍스 잠그기
+            for (const auto& word : current_words) {  // 단어 목록 출력
+                cout << word << endl;
+            }
+            cout << "Enter a word (" << "\"exit\"" << " to quit): ";  // 입력 프롬프트 출력
+            mtx.unlock();  // 뮤텍스 풀기
+        }
+        
+        this_thread::sleep_for(chrono::milliseconds(500));  // 0.5초 대기
+    }
+}
+
+void userInput(vector<string>& current_words, int& miss, int& score, const string& exitWord, WordCreator* wordCreator) {
+    string inputWord;
+    while (true) {
+        //cout << "Enter a word (" << "\"exit\"" << " to quit): ";
+        cin >> inputWord;
+        cin.ignore();
+
+        // exit 입력 시 게임 종료
+        if (inputWord == exitWord) {
+            cout << "Game over!" << endl;
+            cout << "Missed: " << miss << endl;  // 더 이상 이동할 수 없는 단어의 수 출력
+            cout << "Score: " << score << endl;  // 점수 출력
+            exit(0);
+        }
+
+        // 입력한 단어가 목록에 있는지 확인하고, 있다면 해당 단어를 공백으로 변경
+        mtx.lock();  // 뮤텍스 잠그기
+        if (!current_words.back().empty() && current_words.back() != inputWord) {  // 마지막 줄의 단어가 비어있지 않고, 입력한 단어와 다른 경우
+            miss++;  // miss 증가
+        }
+        for (auto it = current_words.begin(); it != current_words.end(); ++it) {
+            size_t pos = it->find(inputWord);
+            if (pos != string::npos) {
+                it->replace(pos, inputWord.length(), string(inputWord.length(), ' '));  // 단어를 공백으로 변경
+                score += 10;  // 점수 증가
+
+                // 맨 아래 줄부터 문자열을 하나씩 위로 올림
+                for (int i = current_words.size() - 1; i > 0; i--) {
+                    current_words[i] = current_words[i - 1];
+                }
+
+                // 맨 위 줄에 새로운 문자열 추가
+                int length = rand() % 8 + 1;  // 단어의 길이를 랜덤하게 생성
+                current_words[0] = string(rand() % 4 * 20, ' ') + wordCreator->createWord(length);
+                break;  // 단어를 찾아서 변경하고 새로운 단어를 추가했으므로 더 이상 반복할 필요 없음
+            }
+        }
+        mtx.unlock();  // 뮤텍스 풀기
+    }
+}
+
 int main() {
     srand(static_cast<unsigned int>(time(0)));
     bool useAlphabet, useNumber, useSpecialCharacter;
@@ -94,70 +182,18 @@ int main() {
         wordCreator = new SpecialCharacterDecorator(wordCreator);
     }
 
-    vector<string> current_words(10, "");  // 10줄의 단어 목록
     vector<string> used_words;  // 사용된 단어들을 저장하는 벡터
     int miss = 0;  // 더 이상 이동할 수 없는 단어의 수
     int score = 0;  // 점수
 
-    // 새 단어 추가
-    for (auto& word : current_words) {
-        int space = rand() % 4 * 20;  // 콘솔창을 x축으로 4등분하여 랜덤한 위치에 문자열 출력
-        int length = rand() % 8 + 1;  // 단어의 길이를 랜덤하게 생성
-        word = string(space, ' ') + wordCreator->createWord(length);
-    }
+    thread dropThread(drop, wordCreator, ref(current_words));  // 문자열 하강 스레드 시작
+    dropThread.detach();
 
-    string inputWord = "";
-    while (true) {
-        cout << "\033[2J\033[1;1H";  // 화면 지우기
+    thread printThread(printWords, ref(current_words));  // 화면 출력 스레드 시작
+    printThread.detach();
 
-        for (const auto& word : current_words) {  // 단어 목록 출력
-            cout << word << endl;
-        }
-
-        cout << "Enter a word (" << "\"exit\"" << " to quit): ";
-        cin >> inputWord;
-        cin.ignore();
-
-        // exit 입력 시 게임 종료
-        if (inputWord == "exit") {
-            cout << "Game over!" << endl;
-            cout << "Missed: " << miss << endl;  // 더 이상 이동할 수 없는 단어의 수 출력
-            cout << "Score: " << score << endl;  // 점수 출력
-            break;
-        }
-
-        // 입력한 단어가 목록에 있는지 확인하고, 있다면 해당 단어를 공백으로 변경 후 줄 이동
-        for (auto it = current_words.begin(); it != current_words.end(); ++it) {
-            size_t pos = it->find(inputWord);
-            if (pos != string::npos) {
-                it->replace(pos, inputWord.length(), string(inputWord.length(), ' '));  // 단어를 공백으로 변경
-                used_words.push_back(inputWord);  // 사용된 단어 추가
-                score += 10;  // 점수 증가
-
-                // 맨 아래 단어가 아니면 맨 아래 단어를 벡터에서 제거하고, 그 위에 있는 단어들을 한 줄씩 아래로 내려오게 하고, miss 증가
-                if (it != current_words.end() - 1) {
-                    current_words.erase(current_words.end() - 1);
-                    miss++;
-
-                    // 줄 이동
-                    for (auto iter = it; iter != current_words.begin(); --iter) {
-                        iter->swap(*(iter - 1));
-                    }
-                }
-                else {
-                    // 맨 아래 단어를 벡터에서 제거
-                    current_words.erase(it);
-                }
-
-                // 새 단어 추가
-                int space = rand() % 4 * 10;  // 콘솔창을 x축으로 4등분하여 랜덤한 위치에 문자열 출력
-                int length = rand() % 8 + 1;  // 단어의 길이를 랜덤하게 생성
-                current_words.insert(current_words.begin(), string(space, ' ') + wordCreator->createWord(length));  // 맨 위에 새 단어 추가
-
-                break;  // 단어를 찾아서 변경하고 새로운 단어를 추가했으므로 더 이상 반복할 필요 없음
-            }
-        }
-    }
+    thread userInputThread(userInput, ref(current_words), ref(miss), ref(score), "exit", wordCreator);  // 사용자 입력 스레드 시작
+    userInputThread.join();
 
     delete wordCreator;  // 메모리 해제
     return 0;
